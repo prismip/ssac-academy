8c778de4a1b2faa1843d1cc53d1a2bf084e67ab6
oh.chi.hooooon <oh.chi.hooooon@gmail.com>
2020-12-21 17:25:44 +0900

    update file

--- workspace/code-workspace/dashboardweb/stocks/urls.py
+++ workspace/code-workspace/dashboardweb/stocks/urls.py
@@ -4,8 +4,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import HomeView
+from .views import HomeView, MarketInfoView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home")
+    path('', HomeView.as_view(), name="home"),
+    path('market/', MarketInfoView.as_view(), name="market"),
 ]
--- workspace/code-workspace/dashboardweb/stocks/views.py
+++ workspace/code-workspace/dashboardweb/stocks/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic.base import View, TemplateView
+
+from django.http import HttpResponse
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = "stocks/home.html"
+
+class MarketInfoView(View):
+    def get(self, request): # View에서 상속 받은 메서드 -> 재정의, 브라우저에서 get 요청을 보내면 호출되는 메서드
+        return HttpResponse("Hello, there", content_type="text/plain;charset=utf-8") # HTML을 응답하는 객체 반환
