63db82196d1b994d12be36fbf90de5609aabfcd3
oh.chi.hooooon <oh.chi.hooooon@gmail.com>
2020-12-21 18:18:36 +0900

    update

--- workspace/code-workspace/dashboardweb/stocks/views.py
+++ workspace/code-workspace/dashboardweb/stocks/views.py
@@ -10,4 +10,15 @@ class HomeView(TemplateView):
 
 class MarketInfoView(View):
     def get(self, request): # View에서 상속 받은 메서드 -> 재정의, 브라우저에서 get 요청을 보내면 호출되는 메서드
-        return HttpResponse("Hello, there", content_type="text/plain;charset=utf-8") # HTML을 응답하는 객체 반환
+        # return HttpResponse("Hello, there !!!!!!!!", content_type="text/plain;charset=utf-8") # HTML을 응답하는 객체 반환
+        import FinanceDataReader as fdr
+        
+        kospi = fdr.DataReader("KS11", '20201221')
+        close_value1 = str(kospi.values[0][0])
+
+        kosdaq = fdr.DataReader("KQ11", '20201221')
+        close_value2 = str(kosdaq.values[0][0])
+
+        return HttpResponse("{0},{1}".format(close_value1, close_value2), 
+                            content_type="text/plain;charset=utf-8") # HTML을 응답하는 객체 반환
+        
