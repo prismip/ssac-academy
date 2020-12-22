6544a8e870fd7942d005f8b631111483de4d242b
oh.chi.hooooon <oh.chi.hooooon@gmail.com>
2020-12-22 12:14:03 +0900

    update

--- workspace/code-workspace/dashboardweb/stocks/urls.py
+++ workspace/code-workspace/dashboardweb/stocks/urls.py
@@ -4,9 +4,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import HomeView, MarketInfoView
+from .views import HomeView, MarketInfoView, SearchView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('market/', MarketInfoView.as_view(), name="market"),
+    path('search/<str:key>', SearchView.as_view(), name="search"),
 ]
--- workspace/code-workspace/dashboardweb/stocks/views.py
+++ workspace/code-workspace/dashboardweb/stocks/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.base import View, TemplateView
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from django.core import serializers
+
+from .models import StockMaster
 
 # Create your views here.
+
 
 class HomeView(TemplateView):
     template_name = "stocks/home.html"
@@ -28,3 +33,9 @@ class MarketInfoView(View):
         return HttpResponse("{0},{1},{2},{3}".format(close_value1, close_value2, close_value3, close_value4), 
                             content_type="text/plain;charset=utf-8") # HTML을 응답하는 객체 반환
         
+class SearchView(View):
+    def get(self, request, key):
+        searched_stocks = StockMaster.objects.filter(name__contains=key) # name like %key%
+        serialized_stocks = serializers.serialize('json', searched_stocks) # serialize : instance -> json string
+        return JsonResponse(serialized_stocks, safe=False, json_dumps_params={'ensure_ascii':False})
+
