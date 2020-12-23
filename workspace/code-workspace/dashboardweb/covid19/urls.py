eb935928bb83d40edef04ec7120420a98d84354e
oh.chi.hooooon <oh.chi.hooooon@gmail.com>
2020-12-23 11:04:36 +0900

    update

--- workspace/code-workspace/dashboardweb/covid19/urls.py
+++ workspace/code-workspace/dashboardweb/covid19/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ConfirmedStatView, HomeView
+from .views import ConfirmedStatView, HomeView, ConfirmedRegionView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path('confirmed/', ConfirmedStatView.as_view(), name="confirmed")
+    path('confirmed/', ConfirmedStatView.as_view(), name="confirmed"),
+    path('confirmed/<str:region>/', ConfirmedRegionView.as_view(), name="confirmed_region")
 ]
--- workspace/code-workspace/dashboardweb/covid19/views.py
+++ workspace/code-workspace/dashboardweb/covid19/views.py
@@ -29,3 +29,31 @@ class ConfirmedStatView(View):
         item_list_json = json.dumps(item_list, ensure_ascii=False)
 
         return HttpResponse(item_list_json, content_type='application/json;charset=utf-8')
+
+class ConfirmedRegionView(View):
+    def get(self, request, region):
+    
+        import requests
+        import xmltodict
+        import json
+        from datetime import datetime, timedelta
+
+        to_date = datetime.today().strftime("%Y%m%d") # 날짜 -> 'yyyymmdd'
+        from_date = (datetime.today() - timedelta(days=10)).strftime("%Y%m%d")
+
+        url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson'
+        api_key = 'R%2BBEwJ49XVmTutdoCAdNq4m7wjOsdFNhVtlzkxWVeZJPhwQqWbGcD7CNeQPipFzIWAanQVw%2FrPLsGkfcYvD4AQ%3D%3D'
+        # http://localhost:8000/a/b/c/?name=x&age=30
+        qs = 'serviceKey={0}&pageNo={1}&numOfRows={2}&startCreateDt={3}&endCreateDt={4}'.format(api_key, 1, 10, from_date, to_date)
+
+        response = requests.get(url + "?" + qs)
+
+        response_dict = xmltodict.parse(response.content) # xml -> ordereddict
+        response_json = json.dumps(response_dict, ensure_ascii=False) # ordereddict -> json-string
+        response_dict2 = json.loads(response_json) # json-string -> general dict
+        item_list = response_dict2['response']['body']['items']['item']
+
+        item_list_by_region = [ item for item in item_list if item['gubun'] == region ]
+        
+        item_list_by_region_json = json.dumps(item_list_by_region, ensure_ascii=False)
+        return HttpResponse(item_list_by_region_json, content_type='application/json;charset=utf-8')
